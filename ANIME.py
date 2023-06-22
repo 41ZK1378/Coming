3b5cba45d170cb418616ba3e930e6646a8b8397b
MUHAMAD_RIZKI <89663540+41ZK1378@users.noreply.github.com>
2023-06-23 02:02:03 +0700

    Update ANIME.py

--- ANIME.py
+++ ANIME.py
@@ -54,7 +54,7 @@ for z in range(200):
 	versi = random.choice(["10_0_2","10_1_1","10_2","10_2_1","10_3_1","10_3_2","10_3_3"])
 	verchrome = random.choice(["602.1.50","602.2.14","602.3.12","602.4.6","603.1.30","603.2.4","603.3.8","601.1.46"])
 	mob = random.choice(["14A456","14B100","14C92","14D27","14E304","14F89","14G60","13C75","13D15","13E233","13E238","13F69","13G34","13G36"])
-	ua = f"Mozilla/5.0 (Linux; Android {versi_android}; TECNO LC8 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{versi_chrome} Mobile Safari/537.36 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/402.0.0.11.10.{str(random.randint(100000, 900000))};]"
+	ua = f"Mozilla/5.0 (Linux; Android {versi_android}; TECNO LC8 Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{versi_chrome} Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/402.0.0.11.10.{str(random.randint(100000, 900000))};]"
 	
 	if ua in ugent:pass
 	else:ugent.append(ua)
